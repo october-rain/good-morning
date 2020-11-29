f906094997ef9c671cbfb0bbd615acc5c9277e49
tian <441101828@qq.com>
2020-11-29 20:05:40 +0800

    后端 自动登录

--- morningsite/morningapp/api.py
+++ morningsite/morningapp/api.py
@@ -5,8 +5,12 @@ from django.contrib.auth.hashers import check_password, make_password
 import jwt
 import time
 
+# token密钥
+salt = "dyxzdh"
 
 # 注册
+
+
 @api_view(['POST'])
 def morn_register(request):
     # print(request.POST)
@@ -15,35 +19,41 @@ def morn_register(request):
     user = User.objects.filter(username=username)
     if user:
         return Response('repeat')
-    new_user = User(username=username,password=password)
+    new_user = User(username=username, password=password)
     new_user.save()
-    token = create_token(username,password)
+    token = create_token(username, password)
     return Response(token)
 
 # 登录
-@api_view(['POST'])
+
+
+@api_view(['POST', 'GET'])
 def morn_login(request):
+    if request.method == 'GET':
+        token = request.POST['token']
+        try:
+            info = jwt.decode(token, salt, True, algorithm='HS256')
+            return Response('ok')
+        except:
+            return Response('error')
     username = request.POST['username']
     password = request.POST['password']
     user = User.objects.filter(username=username)
     if user:
         checkPwd = check_password(password, user[0].password)
         if checkPwd:
-            token = create_token(username,password)
+            token = create_token(username, password)
             return Response(token)
         else:
             return Response('pwderr')
     else:
         return Response('nouser')
-
-# 生成jwt
 def create_token(username, password):
     headers = {
         "alg": "HS256",
         "typ": "JWT"
     }
     # 设置headers，即加密算法的配置
-    salt = "dyxzdh"
     # 随机的salt密钥，只有token生成者（同时也是校验者）自己能有，用于校验生成的token是否合法
     exp = int(time.time() + 60*60)
     # 设置超时时间：当前时间的1s以后超时
@@ -54,6 +64,5 @@ def create_token(username, password):
     # 配置主体信息，一般是登录成功的用户之类的，因为jwt的主体信息很容易被解码，所以不要放敏感信息
     # 当然也可以将敏感信息加密后再放进payload
     token = jwt.encode(headers=headers, payload=payload,
-                   key=salt, algorithm='HS256').decode('utf-8')
+                       key=salt, algorithm='HS256').decode('utf-8')
     return token
-
--- morningsite/morningapp/api_url.py
+++ morningsite/morningapp/api_url.py
@@ -2,5 +2,5 @@ from django.urls import path
 from morningapp import api
 urlpatterns = [
     path('morn-register/',api.morn_register),
-    path('morn-login/',api.morn_login)
+    path('morn-login/',api.morn_login),
 ]
