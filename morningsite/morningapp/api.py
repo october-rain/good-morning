5a469c9f56a9d0f4490fdca469a7336f001238d3
tian <441101828@qq.com>
2020-12-01 12:46:47 +0800

    发布文章功能 简略完成， 还无法对文本图片进行操作

--- morningsite/morningapp/api.py
+++ morningsite/morningapp/api.py
@@ -2,6 +2,8 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.contrib.auth.models import User
 from django.contrib.auth.hashers import check_password, make_password
+from morningapp.models import Article
+from bs4 import BeautifulSoup
 import jwt
 import time
 
@@ -25,8 +27,6 @@ def morn_register(request):
     return Response(token)
 
 # 登录
-
-
 @api_view(['POST', 'GET'])
 def morn_login(request):
     if request.method == 'GET':
@@ -57,6 +57,7 @@ def morn_login(request):
             return Response('pwderr')
     else:
         return Response('nouser')
+# 生成jwtoken
 def create_token(username, password):
     headers = {
         "alg": "HS256",
@@ -75,3 +76,58 @@ def create_token(username, password):
     token = jwt.encode(headers=headers, payload=payload,
                        key=salt, algorithm='HS256').decode('utf-8')
     return token
+
+#保存文章
+@api_view(['POST'])
+def add_article(request):
+    title = request.POST['title']
+    describe = request.POST['describe']
+    cover = request.POST['cover']
+    content = request.POST['content']
+    rep_title = Article.objects.filter(title=title)
+    if rep_title:
+        return Response('repeat')
+    # 保存文章
+    new_article = Article(title=title)
+    # 解析富文本html文档
+    # soup = BeautifulSoup(content, 'html.parser')
+    # # 获取所有img标签 图片
+    # imgList = soup.find_all('img')
+    # for img in range(0, len(imgList)):
+    #     src = imgList[img]['src']
+    #     # print(imgList[img]['src'])
+    #     # 判断图片是远程还是本地
+    #     if 'http://' in src or 'https://' in src:
+    #         # 请求远程图片
+    #         image = requests.get(src)
+    #         # 转化为二进制
+    #         image_data = Image.open(BytesIO(image.content))
+    #         # print(image_data)
+    #         # 设定文件名称
+    #         image_name = datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '-' + \
+    #             str(new_article.id) + '-' + str(img)
+    #         image_data.save("upload/"+image_name+".png")
+    #         new_src = hostURL + 'upload/' + image_name + '.png'
+    #         content = content.replace(src, new_src)
+    #         # 封面设定
+    #         if cover == src:
+    #             cover = new_src
+    #     else:
+    #         image_data = base64.b64decode(src.split(',')[1])
+    #         image_name = datetime.datetime.now().strftime('%Y%m%d%H%M%S') + '-' + str(new_article.id) + \
+    #             '-' + str(img) + '.' + \
+    #             src.split(',')[0].split('/')[1].split(';')[0]
+    #         image_url = os.path.join('upload', image_name).replace('\\', '/')
+    #         with open(image_url, 'wb') as f:
+    #             f.write(image_data)
+    #         new_src = hostURL + image_url
+    #         content = content.replace(src, new_src)
+    #         # 封面设定
+    #         if cover == src:
+
+    #             cover = new_src
+    new_article.content = content
+    new_article.describe = describe
+    new_article.cover = cover
+    new_article.save()
+    return Response('ok')
--- morningsite/morningapp/api_url.py
+++ morningsite/morningapp/api_url.py
@@ -3,4 +3,5 @@ from morningapp import api
 urlpatterns = [
     path('morn-register/',api.morn_register),
     path('morn-login/',api.morn_login),
+    path('add-article/',api.add_article)
 ]
