82ac039ef78eed2f04877e486a99e1f6cc9a5550
tian <441101828@qq.com>
2020-12-08 10:44:38 +0800

    删除后端

--- morningsite/morningapp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from morningapp.models import Article,Profile,Contact,Tag,Tag_Article,Mood
-# Register your models here.
-admin.site.register(Article)
-admin.site.register(Profile)
-admin.site.register(Contact)
-admin.site.register(Tag)
-admin.site.register(Tag_Article)
-admin.site.register(Mood)
