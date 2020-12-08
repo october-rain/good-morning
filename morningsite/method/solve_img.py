61be16a4727bec0c60c9110bd0ba62e54979e384
tian <441101828@qq.com>
2020-12-08 14:31:44 +0800

    文章获取封面完成  添加文章功能基本完成

--- morningsite/method/solve_img.py
+++ morningsite/method/solve_img.py
@@ -1,3 +1,5 @@
+from django.core.files.storage import default_storage
+from django.core.files.base import ContentFile
 import os
 import os.path
 import tinify
@@ -11,21 +13,29 @@ tinify.key = "4WP5qvWSKm7wl4rj2TvCNtGs7vHHqd2L"
 url = "https://api.tian999.top/"
 dir_path = 'upload/'
 
+def compress_img(path):
+    with open(path,'rb') as f:
+        size = len(f.read())
+    if(size/1e3 > 1000):
+        source = tinify.from_file(path)
+        resized = source.resize(method="fit", width=700, height=400)
+    else :
+        pass  
+
 def creat_img(src,article_id):
-    if 'http://' in src or 'https://' in src:
+    print(type(src))
+    if isinstance(src,str):
         # 请求远程图片
         image = requests.get(src)
         # 转化为二进制
         image_data = Image.open(BytesIO(image.content))
         path = dir_path + str(article_id) + '-' + str(datetime.datetime.now().strftime('%Y%m%d%H%M%S')) + '.png'
         image_data.save(path)
-        with open(path,'rb') as f:
-            size = len(f.read())
-        if(size/1e3 > 1000):
-            source = tinify.from_file(path)
-            resized = source.resize(method="fit", width=700, height=400)    
-        address = url + path
+        compress_img(path)
         # resized.to_file(address)
     else :
-        pass
-    return address
+        path = str(article_id) + '-' + str(datetime.datetime.now().strftime('%Y%m%d%H%M%S')) + '.png'
+        address = default_storage.save(path,ContentFile(src.read()))
+        path = dir_path + path
+        compress_img(path)
+    return url + path
